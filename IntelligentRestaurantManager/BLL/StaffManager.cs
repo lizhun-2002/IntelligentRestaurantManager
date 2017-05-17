e506e88e1b79d3cf03f51c92bbd6f7f3cafede5c
lizhun-2002 <supergomtingyi@gmail.com>
2017-05-17 16:47:49 +0900

    bll_staff

---
+++ IntelligentRestaurantManager/BLL/StaffManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntelligentRestaurantManager.Model;
+using IntelligentRestaurantManager.DAL;
+
+namespace IntelligentRestaurantManager.BLL
+{
+    class StaffManager
+    {
+        private StaffService staffService = new StaffService();
+
+        //check if given name is already used
+        public bool CheckExists(string name)
+        {
+            Staff staff = staffService.GetByName(name);
+            if (staff == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int AddNew(Staff staff)
+        {
+            if (this.CheckExists(staff.Name))
+            {
+                return 0;
+            }
+            else
+            {
+                return staffService.AddNew(staff);
+            }
+
+        }
+
+        public int DeleteByRole(string role)
+        {
+            return staffService.DeleteByRole(role);
+        }
+
+        public int DeleteByName(string name)
+        {
+            return staffService.DeleteByName(name);
+        }
+
+        public int DeleteAll()
+        {
+            return staffService.DeleteAll();
+        }
+
+        public int Update(Staff staff)
+        {
+            return staffService.Update(staff);
+        }
+
+        public IEnumerable<Staff> GetAll()
+        {
+            return staffService.GetAll();
+        }
+
+        public IEnumerable<Staff> GetByRole(string role)
+        {
+            return staffService.GetByRole(role);
+        }
+
+        public Staff GetByName(string name)
+        {
+            return staffService.GetByName(name);
+        }
+
+        public int GetCount()
+        {
+            return this.staffService.GetCount();
+        }
+    }
+}
