c8f1fb0cff0c67a72589b90bdf8c54669f063ce2
KimKeonhee <kim.keonhee@kaist.ac.kr>
2017-05-27 17:19:51 +0900

    Updated reservations form

--- IntelligentRestaurantManager/UI/ReservationsForm.Designer.cs
+++ IntelligentRestaurantManager/UI/ReservationsForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace IntelligentRestaurantManager2017
+﻿namespace IntelligentRestaurantManager
 {
     partial class ReservationsForm
     {
@@ -33,151 +33,188 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.timeUd = new System.Windows.Forms.NumericUpDown();
+            this.tableComboBox = new System.Windows.Forms.ComboBox();
+            this.partyNumberUd = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.timeUd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.partyNumberUd)).BeginInit();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Location = new System.Drawing.Point(26, 27);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(13, 14, 13, 14);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Items.AddRange(new object[] {
-            "Johnson, 8, 17:00",
-            "Mabel, 2, 18:00",
-            "Lee, 4, 18:00"});
-            this.listBox1.Location = new System.Drawing.Point(270, 18);
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(386, 27);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(239, 160);
+            this.listBox1.Size = new System.Drawing.Size(340, 238);
             this.listBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(364, 255);
+            this.button1.Location = new System.Drawing.Point(520, 382);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.Size = new System.Drawing.Size(207, 34);
             this.button1.TabIndex = 3;
             this.button1.Text = "Add Reservation";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(364, 184);
+            this.button2.Location = new System.Drawing.Point(520, 276);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.Size = new System.Drawing.Size(207, 34);
             this.button2.TabIndex = 4;
             this.button2.Text = "Remove Reservation";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 217);
+            this.textBox1.Location = new System.Drawing.Point(26, 326);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.Size = new System.Drawing.Size(141, 28);
             this.textBox1.TabIndex = 5;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(124, 217);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(60, 21);
-            this.textBox2.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 195);
+            this.label1.Location = new System.Drawing.Point(23, 292);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 12);
+            this.label1.Size = new System.Drawing.Size(103, 18);
             this.label1.TabIndex = 8;
             this.label1.Text = "Party Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(122, 195);
+            this.label2.Location = new System.Drawing.Point(174, 292);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 12);
+            this.label2.Size = new System.Drawing.Size(71, 18);
             this.label2.TabIndex = 9;
             this.label2.Text = "Number";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(188, 195);
+            this.label3.Location = new System.Drawing.Point(269, 292);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 12);
+            this.label3.Size = new System.Drawing.Size(74, 18);
             this.label3.TabIndex = 10;
             this.label3.Text = "Time(hr)";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(188, 257);
+            this.label4.Location = new System.Drawing.Point(269, 386);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 12);
+            this.label4.Size = new System.Drawing.Size(51, 18);
             this.label4.TabIndex = 12;
             this.label4.Text = "Table";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(18, 255);
+            this.checkBox1.Location = new System.Drawing.Point(26, 382);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(144, 16);
+            this.checkBox1.Size = new System.Drawing.Size(205, 22);
             this.checkBox1.TabIndex = 13;
             this.checkBox1.Text = "Table Auto Allocation";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // numericUpDown1
+            // timeUd
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(190, 217);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(60, 21);
-            this.numericUpDown1.TabIndex = 14;
+            this.timeUd.Location = new System.Drawing.Point(271, 326);
+            this.timeUd.Margin = new System.Windows.Forms.Padding(4);
+            this.timeUd.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.timeUd.Name = "timeUd";
+            this.timeUd.Size = new System.Drawing.Size(86, 28);
+            this.timeUd.TabIndex = 14;
+            this.timeUd.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
             // 
-            // comboBox1
+            // tableComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(231, 253);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(68, 20);
-            this.comboBox1.TabIndex = 15;
-            this.comboBox1.Text = "001";
+            this.tableComboBox.FormattingEnabled = true;
+            this.tableComboBox.Location = new System.Drawing.Point(330, 380);
+            this.tableComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.tableComboBox.Name = "tableComboBox";
+            this.tableComboBox.Size = new System.Drawing.Size(95, 26);
+            this.tableComboBox.TabIndex = 15;
+            this.tableComboBox.Text = "001";
+            // 
+            // partyNumberUd
+            // 
+            this.partyNumberUd.Location = new System.Drawing.Point(175, 326);
+            this.partyNumberUd.Margin = new System.Windows.Forms.Padding(4);
+            this.partyNumberUd.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.partyNumberUd.Name = "partyNumberUd";
+            this.partyNumberUd.Size = new System.Drawing.Size(86, 28);
+            this.partyNumberUd.TabIndex = 16;
+            this.partyNumberUd.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // ReservationsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(516, 290);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(737, 435);
+            this.Controls.Add(this.partyNumberUd);
+            this.Controls.Add(this.tableComboBox);
+            this.Controls.Add(this.timeUd);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.monthCalendar1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ReservationsForm";
             this.Text = "ReservationsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.Load += new System.EventHandler(this.ReservationsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timeUd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.partyNumberUd)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,13 +227,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown timeUd;
+        private System.Windows.Forms.ComboBox tableComboBox;
+        private System.Windows.Forms.NumericUpDown partyNumberUd;
     }
 }
